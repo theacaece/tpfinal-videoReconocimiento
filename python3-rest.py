a8b0f79ba69f3174f62ae0be6e531dd2027496e6
matiasbavauala <matias.bava@uala.com.ar>
2020-03-24 20:38:35 -0300

    Add cors to server

--- python3-rest.py
+++ python3-rest.py
@@ -4,15 +4,18 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from app import start
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/captura",methods=['POST'])
 def entrenamiento_service():
     start()
     result = "Envio de imagen: OK"
     print(result)
-    return result
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8086)
